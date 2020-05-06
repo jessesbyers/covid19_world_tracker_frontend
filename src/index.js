ecc96c59bb42cec5a2a886247c5992ba6fd8a2f2
jessesbyers <jesse.s.byers@gmail.com>
2020-05-06 10:32:03 -0400

    installed dependencies for bootstrap, redux, thunk

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import rootReducer from './reducers/rootReducer'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+  ));
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux'
+
+// import chartsReducer from './chartsReducer'
+
+const rootReducer = combineReducers({
+    test: "test"
+    // charts: chartsReducer,
+    // notices: noticesReducer,
+    // wonders: wondersReducer
+});
+
+export default rootReducer
+
