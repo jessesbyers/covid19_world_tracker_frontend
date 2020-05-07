503ed686a015720928115441cb79823a4471046d
jessesbyers <jesse.s.byers@gmail.com>
2020-05-07 10:12:59 -0400

    updated New container with code for test fetches to backend

--- src/containers/New.js
+++ src/containers/New.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 import CountryDropdown from '../components/CountryDropdown'
 import Button from 'react-bootstrap/Button';
+
+const BASE_URL = "http://localhost:3000"
 
 const New = () => {
     const [countries, setCountries] = useState([]);
  
     useEffect( () => {
+    // DO NOT DELETE: Code for fetching countries directly from database
+        // async function fetchData() {
+        //     await fetch("https://api.covid19api.com/countries")
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log(data)
+        //         setCountries(data)
+        //     })
+        // }
+
+// code for fetching seed data from backend for testing
         async function fetchData() {
-            await fetch("https://api.covid19api.com/countries")
+            await fetch(`${BASE_URL}` + `/countries`)
             .then(response => response.json())
             .then(data => {
                 console.log(data)
@@ -19,9 +32,17 @@ const New = () => {
         fetchData();
     }, []); 
 
+// DO NOT DELETE: Code for fetching countries directly from database
+    // const options = countries.sort((a, b) => (a.Country > b.Country) ? 1 : -1).map( country => {
+    //     return (
+    //         {name: country.Country, value: country.Slug}
+    //     )
+    // })
+
+// code for fetching seed data from backend for testing
     const options = countries.sort((a, b) => (a.Country > b.Country) ? 1 : -1).map( country => {
         return (
-            {name: country.Country, value: country.Slug}
+            {name: country.name, value: country.slug}
         )
     })
 
