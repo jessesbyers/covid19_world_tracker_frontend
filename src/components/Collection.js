b455041b854141cd0ad67c713b924249f9b1f219
jessesbyers <jesse.s.byers@gmail.com>
2020-05-08 10:40:27 -0400

    passed collection props from Collection to Show

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -11,11 +11,12 @@ const Collection = (props) => {
     return (
         <div key={props.collection.id}>
             <NavLink 
-                to={`/collections/${props.collection.id}`}>
+                to={{
+                    pathname: `/collections/${props.collection.id}`,
+                    collection: {id: props.collection.id, name: props.collection.name, summary: props.collection.summary}}}>
                 <Button 
-                    variant="dark"
-                    >
-                        {props.collection.name}
+                    variant="dark">
+                    {props.collection.name}
                 </Button>
             </NavLink>
             <p>{props.collection.summary}</p>
--- src/containers/Show.js
+++ src/containers/Show.js
@@ -1,11 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import { BASE_URL } from '../constants/constants'
+
+
 
 const Show = (props) => {
+    console.log(props)
+    console.log(`${BASE_URL}` + `/collections/${props.match.params.id}/countries`)
+    const [data, setData] = useState([])
+
+    useEffect( () => {
+        // DO NOT DELETE: Code for fetching countries directly from database
+            // async function fetchData() {
+            //     await fetch("https://api.covid19api.com/countries")
+            //     .then(response => response.json())
+            //     .then(data => {
+            //         console.log(data)
+            //         setCountries(data)
+            //     })
+            // }
+    
+    // code for fetching seed data from backend for testing
+            async function fetchData() {
+                await fetch(`${BASE_URL}` + `/collections/${props.match.params.id}`)
+                .then(response => response.json())
+                .then(results => {
+                    console.log(results)
+                    setData(results)
+                })
+            }
+    
+            fetchData();
+        }, []); 
 
     return (
         <div>
             <h1>SHOW component placeholder</h1>
             <p>renders 4 D3 chart objects, each showing data for a single country in the collection</p>
+            <ul>
+                <li>id: {props.location.collection.id}</li>
+                <li>name: {props.location.collection.name}</li>
+                <li>summary: {props.location.collection.summary}</li>
+            </ul>
         </div>
     )
 }
