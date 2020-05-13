262dbce0b6e1e9f3cc90fb39df8ab88250ade565
jessesbyers <jesse.s.byers@gmail.com>
2020-05-13 09:00:48 -0400

    building line graph on Draw.js

--- src/d3/Draw.js
+++ src/d3/Draw.js
@@ -1,5 +1,7 @@
 import * as d3 from 'd3'
-import React from 'react'
+import {
+    axisBottom, axisLeft, line
+} from 'd3'
 
 // TO DOS:
     // make svg size responsive
@@ -12,18 +14,126 @@ const Draw = (countryName, totalCases, dailyData) => {
     // const formattedDate = new Date(dailyData[0][0].Date).toLocaleDateString()
     console.log(d3)
 
-    // const width = Math.max(document.documentElement.clientWidth, window.innerWidth)
-    // const height = Math.max(document.documentElement.clientHeight, window.innerHeight)
-    const width = 700
-    const height = 500
+    // setting up constants for sizes
+    const width = 100
+    const height = 100
+    const padding = 10
+    const margin = 10
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
 
+    // const xValue = dailyData => d.timestamp;
+    // const yValue
 
-    d3.select(".viz").append("svg")
-    // set height and width to be responsive
-        // .attr("viewBox", `0 0 width height`)
-        .attr("height", height)
-        .attr("width", width)
+    // // setting up svg element
+        const svg = d3.select(".viz")
+        .append("svg")
+        .attr("viewBox", [0, 0, width, height])
         .attr("id", "svg-viz")
+
+
+    const g = svg.append("g")
+        .attr('transform', `translate(${margin.left},${margin.top})`)
+
+
+    // // setting up scales dynamically
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(dailyData, (d, i) => i)])
+        .range([padding, width - padding]);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(dailyData, d => d.Confirmed)])
+        .range([height - padding, padding])
+
+    const yAxisLabel = "Number of Cases"
+    const xAxisLabel = "Day Number"
+    const title = countryName + " Covid-19 Cases"
+
+
+    // setting up axes
+    const xAxis = axisBottom(xScale)
+        // .tickSize(-innerHeight)
+        // .tickPadding(15);
+
+    const yAxis = axisLeft(yScale)
+        // .tickSize(-innerWidth)
+        // .tickPadding(10);
+
+    const yAxisG = g.append('g').call(yAxis);
+        yAxisG.selectAll('.domain').remove();
+
+    yAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', -60)
+        .attr('x', -innerHeight / 2)
+        .attr('fill', 'black')
+        .attr('transform', `rotate(-90)`)
+        .attr('text-anchor', 'middle')
+        .text(yAxisLabel);
+
+    const xAxisG = g.append('g').call(xAxis)
+        .attr('transform', `translate(0,${innerHeight})`);
+      
+    xAxisG.select('.domain').remove();
+    
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 80)
+        .attr('x', innerWidth / 2)
+        .attr('fill', 'black')
+        .text(xAxisLabel);
+
+
+
+
+
+
+
+    const lineGenerator = line()
+        // .x(d => xScale(xValue(d)))
+        .x((d, i) => xScale(i + 1))
+        // .y(d => yScale(yValue(d)))
+        .y(d => yScale(dailyData.map(d => d.Confirmed)))
+        // .curve(curveBasis);
+        
+    g.append('path')
+        .attr('class', 'line-path')
+        .attr('d', lineGenerator(dailyData));
+    
+    g.append('text')
+        .attr('class', 'title')
+        .attr('y', -10)
+        .text(title);
+
+
+
+    
+
+    
+
+
+
+
+
+
+    //     .selectAll("g")
+    //     .data(dailyData)
+    //     .join("g")
+    //          // .attr("fill", d => color(d.key))
+    //         .attr("fill", "steelblue")
+    //     .selectAll("rect")
+    //         .data(d => d)
+    //         .join("rect")
+    //         .attr("x", (d, i) => x(i + 1))
+    //         .attr("y", d => y(d.Confirmed))
+    //         .attr("height", d => y(d.Confirmed))
+    //         .attr("width", 10)
+    //         .enter()
+    //         .append("rect")
+
+
+
+    
 
 
 
