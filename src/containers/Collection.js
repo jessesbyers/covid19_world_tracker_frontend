864a150afbf505b3bfe775bd4ad6006aaef321ba
github username <github email address>
2020-05-18 08:38:35 -0400

    building responsive columns for cards on Collection.js

--- src/containers/Collection.js
+++ src/containers/Collection.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import Country from '../components/Country'
 import { Card } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+import {Row} from 'react-bootstrap'
 
 
 const Collection = (props) => {
         if (props.location.countryData) {
             return (
-                props.location.countryData.map((country, index) => { 
-                    return (
-                        <Card>
-                            <Country id={index} country={country} /> 
-                        </Card>
-                    )
-                })
+                <Row >
+
+                    {props.location.countryData.map((country, index) => { 
+                        return (
+                            <Col sm={12} md={4} lg={3}>
+
+                                <Card>
+                                    <Country id={index} country={country} /> 
+                                </Card>
+                            </Col>
+
+                        )
+                    })}
+                </Row>
+
             )
         } else {
             return <div>Return to Home</div>
