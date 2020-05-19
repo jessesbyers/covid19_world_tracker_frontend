f5f282a8d433b8a399ec61705b9713ca17c3d8b9
github username <github email address>
2020-05-19 13:28:28 -0400

    added escape on Show.js

--- src/containers/Show.js
+++ src/containers/Show.js
@@ -7,11 +7,11 @@ import Viz from '../d3/Viz'
 const Show = (props) => {
     console.log(props)
 
-    // return <VizShow countryName={props.location.countryName} totalCases={props.location.totalCases} dailyData={props.location.dailyData} id={props.location.slug} slug={props.location.slug} />
-    return <Viz countryName={props.location.countryName} totalCases={props.location.totalCases} dailyData={props.location.dailyData} id={props.location.slug} slug={props.location.slug}/>
-    // return (
-    //     <div>TEST</div>
-    // )
+    if (props.location.countryName) {
+        return <Viz countryName={props.location.countryName} totalCases={props.location.totalCases} dailyData={props.location.dailyData} id={props.location.slug} slug={props.location.slug}/>
+    } else {
+        return null
+    }
 }
 
 export default Show
