b616b045da0874b567543c299889ab445b6cc2ab
github username <github email address>
2020-05-19 20:46:51 -0400

    fetched total case data for map on Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,9 +3,32 @@ import DrawMap from '../d3/DrawMap'
 
 
 const Home = (props) => {
+    const [countryResults, setCountryResults] = useState([])
+    console.log(countryResults)
+
 
     useEffect( () => {
-        DrawMap()
+
+        async function fetchData() {
+            var requestOptions = {
+                method: 'GET',
+                redirect: 'follow',
+                headers : { 
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                   }
+              }
+
+            await fetch('/api/country_region', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.results)
+                setCountryResults(data.results)
+            })
+        }
+        fetchData();
+
+        DrawMap(); 
     }, [])
 
     return <div className="mapviz"></div>
@@ -17,7 +40,7 @@ export default Home
 <h3>To Dos...</h3>
 <ul>
     <li>render About / Instructions</li>
-    <li>create covid map component for home</li>
+    <li>add case data to covid map component for home</li>
     <li>add forward and back navlinks to NavBar</li>
     <li>add background image for pages if no data present</li>
     <li>fix sizing on show page</li>
