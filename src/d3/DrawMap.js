1c8664c2abe5b9278e3704b50826aa21c36b1115
github username <github email address>
2020-05-20 08:14:53 -0400

    added hover and country name to DrawMap.js

--- src/d3/DrawMap.js
+++ src/d3/DrawMap.js
@@ -1,4 +1,4 @@
-import { select, json, geoPath, geoNaturalEarth1 } from 'd3';
+import { select, json, geoPath, geoNaturalEarth1, tsv } from 'd3';
 import { feature } from 'topojson';
 
 const DrawMap = () => {
@@ -23,16 +23,25 @@ const DrawMap = () => {
         .attr('class', 'sphere')
         .attr('d', pathGenerator({type: 'Sphere'}));
 
-    json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
-    .then(data => {
-        const countries = feature(data, data.objects.countries);
-        console.log(countries)
 
+    Promise.all([
+        tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
+        json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+    ]).then(([ tsvData, topoJSONdata ]) => {
+
+        const countryName = {}
+        tsvData.forEach(d => {
+            countryName[d.iso_n3] = d.name
+        })
+
+        const countries = feature(topoJSONdata, topoJSONdata.objects.countries);
         svg.selectAll('path').data(countries.features)
         .enter().append('path')
             .attr('class', 'country')
-            .attr('d', pathGenerator);
-    });
+            .attr('d', pathGenerator)
+        .append("title")
+            .text(d => countryName[d.id])
+    })
 }
 
 export default DrawMap
