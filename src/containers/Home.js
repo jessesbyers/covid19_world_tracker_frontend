ec69a89d3435802d047f3c67a4b213ddc583a199
jessesbyers <jesse.s.byers@gmail.com>
2020-05-27 11:29:01 -0400

    updated notes on Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -36,10 +36,7 @@ export default Home
 <h3>To Dos...</h3>
 <ul>
     <li>render About / Instructions</li>
-    <li>add forward and back navlinks to NavBar</li>
     <li>fix sizing on all pages</li>
-    <li>center vertical justify map legends</li>
-    <li>identify countries with missing data (shading)</li>
-    <li>add zoom to bar charts</li>
     <li>fix rendering lag bug on Map (DisplayMapData)</li>
+    <li>collapse button divs/spans into dropdown on small screens</li>
 </ul> */} 
