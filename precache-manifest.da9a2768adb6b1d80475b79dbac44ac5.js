f2ab7ada6ef0c91ec4bd44d9a9a13a72019faa57
jessesbyers <jesse.s.byers@gmail.com>
2020-05-28 10:13:21 -0400

    Updates

---
+++ precache-manifest.da9a2768adb6b1d80475b79dbac44ac5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfed2242736148a3636f308bc674514",
+    "url": "/covid19_world_tracker_frontend/index.html"
+  },
+  {
+    "revision": "edc3112f6ba5226ebcf1",
+    "url": "/covid19_world_tracker_frontend/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1c6d956e1c54013c7511",
+    "url": "/covid19_world_tracker_frontend/static/css/main.fc4a824f.chunk.css"
+  },
+  {
+    "revision": "edc3112f6ba5226ebcf1",
+    "url": "/covid19_world_tracker_frontend/static/js/2.1e654d07.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19_world_tracker_frontend/static/js/2.1e654d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6d956e1c54013c7511",
+    "url": "/covid19_world_tracker_frontend/static/js/main.df9554b6.chunk.js"
+  },
+  {
+    "revision": "17b453806fc2a521201c",
+    "url": "/covid19_world_tracker_frontend/static/js/runtime-main.805caea6.js"
+  }
+]);
