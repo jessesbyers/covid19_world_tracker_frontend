fc752054f17e822f62771d81a08f3407c02a335b
jessesbyers <jesse.s.byers@gmail.com>
2020-05-28 10:41:49 -0400

    added HashRouter to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, HashRouter, Link } from 'react-router-dom';
 
 import './App.css';
 
@@ -13,18 +13,20 @@ import Collection from './containers/Collection'
 import Container from 'react-bootstrap/Container';
 
 function App() {
-  return (      
-    <Router >
-      <Container fluid >
-        <div >
-          <NavBar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/countries/:slug" component={Show} />
-          <Route exact path={`/new`} component={New} />
-          <Route exact path={`/collection`} component={Collection} />
-        </div>
-      </Container>
-    </Router>
+  return (    
+    <HashRouter basename='/'> 
+      <Router >
+        <Container fluid >
+          <div >
+            <NavBar />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/countries/:slug" component={Show} />
+            <Route exact path={`/new`} component={New} />
+            <Route exact path={`/collection`} component={Collection} />
+          </div>
+        </Container>
+      </Router>
+    </HashRouter> 
   );
 }
 
