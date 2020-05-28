568b611727f65e957d9d13d1fa5721a2a8ed412a
jessesbyers <jesse.s.byers@gmail.com>
2020-05-28 10:04:47 -0400

    added gh pages dependency

--- src/components/MapInput.js
+++ src/components/MapInput.js
@@ -22,7 +22,7 @@ const MapInput = () => {
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
             this.classList.toggle("active");
-            
+
             if (collButton.innerText === "Open Map Menu") {
                 collButton.innerText = "Close Map Menu"
             } else {
