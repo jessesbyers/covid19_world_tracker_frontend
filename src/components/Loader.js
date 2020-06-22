81bf8692ec5de2a528a55d9415cfbd087b683450
jessesbyers <jesse.s.byers@gmail.com>
2020-06-22 13:43:23 -0400

    added Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Loader = () => {
+    return (                 
+        <button className="dark">LOADING DATA...</button>
+    )
+}
--- src/components/MapInput.js
+++ src/components/MapInput.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import DisplayMapData from '../d3/DisplayMapData'
+import { Loader } from '../components/Loader'
 
 const MapInput = () => {
 
@@ -44,39 +45,40 @@ const MapInput = () => {
 
 
     return (
-
-
-        <span>
-
-            <button className="collapsible1">Close Map Menu</button>
-
+        <div>
+            {/* loading */}
             {isLoading ? (
-                <div>Loading...</div>
+                < Loader />
+                // <button className="dark">LOADING DATA...</button>
             ) : (
 
-            <div className="content1">
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="cases block" value="cases">Total Cases</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="casesPerOneMillion block" value="casesPerOneMillion">Total Cases Per Million</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="todayCases block" value="todayCases">New Cases Today</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="deaths block" value="deaths">Total Deaths</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="deathsPerOneMillion block" value="deathsPerOneMillion">Deaths Per Million</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="todayDeaths block" value="todayDeaths">New Deaths Today</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="activee block" value="active">Active Cases</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="activePerOneMillion block" value="activePerOneMillion">Active Cases Per Million</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="recovered block" value="recovered">Recovered Cases</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="recoveredPerOneMillion block" value="recoveredPerOneMillion">Recovered Cases Per Million</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="critical block" value="critical">Critical Cases</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="criticalPerOneMillion block" value="criticalPerOneMillion">Critical Cases Per Million</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="tests block" value="tests">Number of Tests</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="testsPerOneMillion block" value="testsPerOneMillion">Tests Per Million</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="population block" value="population">Population</button>
-                <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="reset block" value="reset">Reset Map</button>
-            </div>
+            <span>
+
+                <button className="collapsible1">Close Map Menu</button>
+
+                <div className="content1">
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="cases block" value="cases">Total Cases</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="casesPerOneMillion block" value="casesPerOneMillion">Total Cases Per Million</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="todayCases block" value="todayCases">New Cases Today</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="deaths block" value="deaths">Total Deaths</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="deathsPerOneMillion block" value="deathsPerOneMillion">Deaths Per Million</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="todayDeaths block" value="todayDeaths">New Deaths Today</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="activee block" value="active">Active Cases</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="activePerOneMillion block" value="activePerOneMillion">Active Cases Per Million</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="recovered block" value="recovered">Recovered Cases</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="recoveredPerOneMillion block" value="recoveredPerOneMillion">Recovered Cases Per Million</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="critical block" value="critical">Critical Cases</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="criticalPerOneMillion block" value="criticalPerOneMillion">Critical Cases Per Million</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="tests block" value="tests">Number of Tests</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="testsPerOneMillion block" value="testsPerOneMillion">Tests Per Million</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="population block" value="population">Population</button>
+                    <button onClick={event => DisplayMapData(event.target.value, event.target.innerText, countryResults)} className="reset block" value="reset">Reset Map</button>
+                </div>
+
+            </span>
 
             )}
-        </span>
-
-       
+        </div>
     )
 }
 
