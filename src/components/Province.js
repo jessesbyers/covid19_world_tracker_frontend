cee1d3e0a622644efe06138b0b131da9099c0654
Jesse Byers <jesse.s.byers@gmail.com>
2020-07-05 14:21:56 -0400

    added more specificity to slug function in Province.js

--- src/components/Province.js
+++ src/components/Province.js
@@ -6,8 +6,19 @@ import { Loader } from './Loader'
 
 const Province = ({caseType, province, provinceData}) => {
     console.log(provinceData)
-    const slug = province.split(" ").map(word => word.toLowerCase()).join("-")
+    // const slug = province.split(" ").map(word => word.toLowerCase()).join("-")
     const totalCases = () => provinceData.length === 0 ? 0 : provinceData[provinceData.length-1].Confirmed
+    const slug = (province) => {
+        return province
+        .toString()
+        .trim()
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        .replace(/[^\w\-]+/g, "")
+        .replace(/\-\-+/g, "-")
+        .replace(/^-+/, "")
+        .replace(/-+$/, "");
+    }
 
         const array = []
         const parseData = (provinceData, array) => {
@@ -31,7 +42,7 @@ const Province = ({caseType, province, provinceData}) => {
                 //     pathname: `/countries/${slug}`,
                 //     slug
                 //     }}>
-                    <Viz countryName={province} totalCases={totalCases()} dailyData={parseData(provinceData, array)} caseType={caseType} slug={slug}/>
+                    <Viz countryName={province} totalCases={totalCases()} dailyData={parseData(provinceData, array)} caseType={caseType} slug={slug(province)}/>
                 // </NavLink>
         )
     } else {
